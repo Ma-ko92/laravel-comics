c53b6d3b0e076e735455b70e70624bbcb0be453f
Mario Koco <mario.koco92@gmail.com>
2021-06-08 21:14:19 +0200

    header completo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    {{-- Title --}}
+    <title>DC | Welcome to DC</title>
+    {{-- Icon --}}
+    <link rel="icon" href=" {{ asset('img/favicon.ico') }} ">
 
+    {{-- Google font --}}
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    {{-- Fontawesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- Css --}}
     <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
 </head>
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,66 @@
 {{-- Header --}}
 <header>
-   Header
+   {{-- Top header --}}
+   <div class="top_header">
+      <div class="container">
+         {{-- &nbsp; codice che permette di avere un pò di spazio extra tra le parole, usato perchè altrimenti poco leggibile --}}
+         <a href="#">dc power<span>sm</span>&nbsp; visa®</a>
+         <a href="#">additional&nbsp;DC&nbsp; sites <i class="fas fa-sort-down"></i></a>
+      </div>
+   </div>
+
+   {{-- Nav --}}
+   <div class="container">
+      {{-- Flex container --}}
+      <div class="flex_container">
+         {{-- Logo --}}
+         <div class="logo">
+            <a href="/">
+               <img src="{{ asset('img/dc-logo.png') }}" alt="DC logo">
+            </a>
+         </div>
+
+         {{-- Menu --}}
+         <ul class="menu">
+            <li>
+               <a href="#">Characters</a>
+            </li>
+            <li class="active">
+               <a href="#">Comics</a>
+            </li>
+            <li>
+               <a href="#">Movies</a>
+            </li>
+            <li>
+               <a href="#">Tv</a>
+            </li>
+            <li>
+               <a href="#">Games</a>
+            </li>
+            <li>
+               <a href="#">Collectibles</a>
+            </li>
+            <li>
+               <a href="#">Videos</a>
+            </li>
+            <li>
+               <a href="#">Fans</a>
+            </li>
+            <li>
+               <a href="#">News</a>
+            </li>
+            <li>
+               <a href="#">Shop <i class="fas fa-sort-down"></i></a>
+            </li>
+         </ul>
+         {{-- Menu End --}}
+
+         {{-- Search bar --}}
+         <div class="search">
+            <input type="text" placeholder="Search"><i class="fas fa-search"></i>
+         </div>
+      </div>
+      {{-- Flex container End --}}
+   </div>
 </header>
 {{-- Header end--}}
