cffebaedeb228a9e7540700d448a7ee7607e5274
Mario Koco <mario.koco92@gmail.com>
2021-06-09 00:55:04 +0200

    importato array comics

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,7 +3,23 @@
 
 {{-- Main --}}
 @section('main_content')
+    {{-- Jumbotron --}}
     <section>
         <div class="jumbotron"></div>
+    </section>
+
+    {{-- Main content --}}
+    <section>
+        <div class="comics_container">
+            <div class="container">
+                {{-- Comics --}}
+                <div class="comics">
+                    {{-- Comic --}}
+                    <div class="comic">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
